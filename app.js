1d182f454e7b9fded1b0a8e42f5433571cb55e28
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-04-16 19:20:49 +0700

    add users routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv/config");
 const express = require("express");
 const morgan = require("morgan");
 
+//routes
+const userRoutes = require("./app/routes/users");
+
 const port = process.env.APP_PORT || 3002;
 const app = express();
 
@@ -11,9 +14,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //router
+const apiUrl = "/api/v1";
+app.use(apiUrl, userRoutes);
+
 app.get("/test", (req, res) => {
   res.json({
     message: "oke boooks",
+  });
+});
+
+//handling error
+app.use((req, res, next) => {
+  const err = new Error(
+    `${req.method} => ${req.hostname}${req.path} not found`
+  );
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "developmpnet" ? err : {};
+  res.status(err.status || 500).json({
+    status: "failed",
+    message: err.message,
   });
 });
 
